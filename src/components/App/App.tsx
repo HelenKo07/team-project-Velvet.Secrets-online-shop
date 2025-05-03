641afcef379bfee3ecb702388d0f6b2b43635930
Alena_Korma <alena.gorova93@gmail.com>
2025-05-04 01:27:05 +0300

    commit message

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,18 @@
-import { Routes, Route } from 'react-router-dom'
-import MainLayout from '../../layouts/MainLayout'
-import HomePage from '../../pages/HomePage/HomePage'
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import MainLayout from '../../layouts/MainLayout';
+import HomePage from '../../pages/HomePage/HomePage';
+import ProductsPages from '../../pages/ProductsPage/ProductsPage';
+import './App.css';
 
 function App() {
-  
-
   return (
-      <MainLayout>
-        <Routes>
+    <MainLayout>
+      <Routes>
         <Route path="/" element={<HomePage />} />
-        </Routes>
-        
-      </MainLayout>
-  )
+        <Route path="/products" element={<ProductsPages />} />
+      </Routes>
+    </MainLayout>
+  );
 }
 
-export default App
+export default App;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import Footer from "../components/Footer/Footer";
+import Footer from '../components/Footer/Footer';
 import Header from "../components/Header/Header";
 
 interface MainLayoutProps {
