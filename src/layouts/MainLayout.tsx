07b60171e8a15aa87483ea7f6c3c704b62b40a8a
Alena_Korma <alena.gorova93@gmail.com>
2025-05-03 21:36:48 +0300

    commit message

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,18 @@
+// import { FC } from "react";
+// import Footer from "../components/Footer/Footer";
+// import Header from "../components/Header/Header";
+
+// interface MainLayoutProps {
+// children: React.ReactNode;
+// }
+
+// const MainLayout: FC<MainLayoutProps> = ({children}) => {
+// return (
+//     <>
+//     <Header/>
+//     <main> {children} </main>
+//     <Footer/>
+//     </> 
+// );
+// }
+// export default MainLayout;
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,16 @@
+export default function HomePage () {
+    return (
+        <div>
+            <section className="">
+                <h2>Бути собою — твій головний тренд</h2>
+                <p>Обирай одяг, який підкреслює твою унікальність. Без правил. Без компромісів.</p>
+            </section>
+            <section>
+                <div>Image</div>
+                <div>Content</div>
+            </section>
+            //разделить по секции и сюда импортировать по секциям
+            
+        </div>
+    );
+}
